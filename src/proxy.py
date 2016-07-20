ec6115f5c8949cf1445fd349963419d288cd9469
johnsonyue <johnsonyuehit@163.com>
2016-07-20 15:38:57 +0000

    added multiprocess

--- src/proxy.py
+++ src/proxy.py
@@ -1,5 +1,7 @@
 import HTMLParser
 import urllib2
+import socket
+from multiprocessing import Pool
 
 import sys
 reload(sys);
@@ -72,15 +74,20 @@ class ProxyParser(HTMLParser.HTMLParser):
 			self.is_page = False;
 
 class ProxyPool():
-	def __init__(self):
+	def __init__(self, file=""):
 		self.seed_url = "http://www.xicidaili.com/nt/";
-		self.parser = ProxyParser();
-		request = urllib2.Request(self.seed_url);
-		request.add_header('User-agent','Mozilla/5.0');
-		f = urllib2.urlopen(request);
-		text = f.read();
-		self.parser.feed(text);
-	
+		self.test_url = "http://data.caida.org/datasets/topology/ark/";
+		self.proxy_list = [];
+		self.ip_dict = {};
+		if (file != ""):
+			f = open(file);
+			i = 0;
+			for line in f.readlines():
+				svr = line.strip('\n');
+				self.proxy_list.append(svr);
+				self.ip_dict[svr] = i;
+				i = i + 1;
+			
 	def translate_uptime(self, uptime):
 		num = u"";
 		unit = u"";
@@ -99,16 +106,111 @@ class ProxyPool():
 				res.append(self.parser.ip[i]+":"+self.parser.port[i]);
 		return res;
 	
-	def get_usable_proxy(self, list):
-		return "";
+	def test_proxy(self, server):
+		proxy = "http://"+server;
+		opener = urllib2.build_opener(urllib2.ProxyHandler({"http":proxy}));
+		code = 0;
+		res = False;
+		try:
+			code = opener.open(self.test_url, timeout = 5).getcode();
+		except urllib2.HTTPError ,e:
+			res = False;
+		except urllib2.URLError, e:
+			res = False;
+		except socket.timeout, e:
+			res = False;
+		except socket.error, e:
+			res = False;
 
-pool = ProxyPool();
-print pool.get_candidate_proxy();
-parser = ProxyParser(True);
-seed_url = "http://www.xicidaili.com/nt/";
-request = urllib2.Request(seed_url);
-request.add_header('User-agent','Mozilla/5.0');
-f = urllib2.urlopen(request);
-text = f.read();
-parser.feed(text);
-print parser.page_num;
+		if (code == 200):
+			res = True;
+		
+		if (res_list != None and res_ind != -1):
+			res_list[res_ind] = res;
+		
+		print (server+" "+str(res));
+
+		return res;
+		
+	
+	def test_list(self, svr_list, mp_num=1):
+		ret = [];
+		if (mp_num > 1):
+			p = Pool(mp_num);
+			res_list = [False for i in range(len(svr_list))];
+
+			for i in range(len(svr_list)):
+				svr = svr_list[i];
+				res = p.apply_async(self.test_proxy, args=(svr, ));
+				if (res):
+					ret.append(svr);
+
+			p.close();
+			p.join();
+		
+		elif(mp_num == 1):
+			for i in range(len(svr_list)):
+				svr = svr_list[i];
+				if (self.test_proxy(svr)):
+					ret.append(svr);
+		
+		return ret;
+	
+	def update_proxy(self, check_legacy = False, max_page_num = 0, mp_num = 1):
+		#updating legacy list.
+		if (check_legacy):
+			print "legacy list";
+			self.proxy_list = self.test_list(self.proxy_list, mp_num);
+			self.ip_dict = {};
+			for i in range(len(self.proxy_list)):
+				svr = self.proxy_list[i];
+				self.ip_dict[svr] = i;
+			print "legacy list finished testing.";
+
+		#updating from xici.com.
+		total_page = 1;
+		cur_page = 1;
+		while(cur_page <= total_page):
+			print "page #"+str(cur_page);
+			if (cur_page == 1):
+				self.parser = ProxyParser(True);
+				url = self.seed_url;
+			else:
+				self.parser = ProxyParser();
+				url = self.seed_url+"/"+str(cur_page);
+			#added user agent segment to avoid http 500 error.
+			request = urllib2.Request(url);
+			request.add_header('User-agent','Mozilla/5.0');
+			f = urllib2.urlopen(request);
+			text = f.read();
+			self.parser.feed(text);
+			if (cur_page == 1 and max_page_num <= 0):
+				total_page = self.parser.page_num[-1];
+			elif (cur_page == 1):
+				total_page = max_page_num;
+
+			svr_list = self.get_candidate_proxy();
+			res_list = self.test_list(svr_list, mp_num);
+			
+			#adding to proxy_list.
+			for i in range(len(res_list)):
+				svr = res_list[i];
+				if (not self.ip_dict.has_key(svr)):
+					self.proxy_list.append(svr);
+					self.ip_dict[svr] = len(self.proxy_list)-1;
+
+			print "page #"+str(cur_page)+" finished testing,"+str(len(res_list))+"/"+str(len(svr_list))+" true.";
+			print "total usable proxy ip num: "+str(len(self.proxy_list));
+
+			cur_page = cur_page + 1;
+						
+	
+	def export_proxy(self, file_name):
+		f = open(file_name, 'wb');
+		for svr in self.proxy_list:
+			f.write(svr+'\n');
+		f.close();
+		
+pool = ProxyPool("proxy_list");
+pool.update_proxy(False, 400, 4);
+pool.export_proxy("proxy_list");
